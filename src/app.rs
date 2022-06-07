cba6fecea1b69592ab53524a8281c543dddc2c7e
last59s <last59s@hotmail.com>
2022-06-07 23:35:04 +0800

    同颜色禁止移动

--- src/app.rs
+++ src/app.rs
@@ -6,25 +6,25 @@ use std::collections::HashMap;
 
 // #[derive(Debug)]
 // #[derive(Clone)]
-struct Piece {
+pub(crate) struct Piece {
     // id: u8,
     // selected: bool,
     // x: f32,
     // y: f32,
-    img: Image,
-    // color: char,
-    vec: Vec2,
+    pub(crate) img: Image,
+    // pub(crate) color: char,
+    pub(crate) loc: Vec2,
 }
 // #[derive(Debug)]
-pub struct Game {
+pub(crate) struct Game {
     board: Image,
     at: Image, // 选中框
     red: HashMap<u8, Piece>,
     black: HashMap<u8, Piece>,
-    m: Vec2,     // 鼠标绝对坐标
-    select: u8,  //(id，颜色)
-    state: bool, // 状态
-    player: u8,  // 玩家(0 & 1)
+    m: Vec2,            // 鼠标绝对坐标
+    select: (u8, char), //(id，颜色)
+    state: bool,        // 状态
+    player: u8,         // 玩家(0 & 1)
 }
 impl Game {
     pub fn new(ctx: &mut Context) -> GameResult<Game> {
@@ -35,7 +35,7 @@ impl Game {
             at,
             red: HashMap::new(),
             black: HashMap::new(),
-            select: 0,
+            select: (0, 'n'),
             state: false,
             m: Vec2::new(-60., -60.),
             player: 0,
@@ -65,7 +65,7 @@ impl Game {
                 // y: y[id as usize],
                 img,
                 // color: c,
-                vec: Vec2::new(x[id as usize] - 24., y[id as usize] - 24.),
+                loc: Vec2::new(x[id as usize] - 24., y[id as usize] - 24.),
             };
 
             if c == 'r' {
@@ -75,8 +75,8 @@ impl Game {
             }
         }
     }
-    // 返回棋子ip，并设置状态ture
-    fn piece_select(&mut self, color: char) -> bool {
+    // 依据id寻找棋子，设置状态ture
+    fn piece_find(&mut self, color: char) {
         let p;
         if color == 'r' {
             p = self.red.iter();
@@ -84,36 +84,47 @@ impl Game {
             p = self.black.iter();
         }
         for (id, p) in p {
-            if p.vec.x + 24. == self.m.x && p.vec.y + 24. == self.m.y {
-                self.select = *id;
+            if p.loc.x + 24. == self.m.x && p.loc.y + 24. == self.m.y {
+                self.select = (*id, color);
                 self.state = true;
-                println!("{}:{}", color, self.select);
-                return true;
+                println!("{}:{}", color, self.select.0);
+                return;
             }
         }
-        false
+        self.select.1 = 'n';
     }
     // 移动棋子，并设置状态为false
-    fn piece_move(&mut self, color: char) -> Option<()> {
-        // 吃掉对方棋子
-        let i = self.select;
+    fn piece_move(&mut self, color: char) {
+        // 吃掉对方棋子 && 同颜色禁止移动
+        let i = self.select.0;
         let p;
         if color == 'r' {
-            self.piece_find_and_delect('b');
-            p = self.red.get_mut(&i)?;
+            for (_, p) in self.red.iter() {
+                if p.loc.x + 24. == self.m.x && p.loc.y + 24. == self.m.y {
+                    return;
+                }
+            }
+            self.piece_remove('b');
+            p = self.red.get_mut(&i).unwrap();
         } else {
-            self.piece_find_and_delect('r');
-            p = self.black.get_mut(&i)?;
+            for (_, p) in self.black.iter() {
+                if p.loc.x + 24. == self.m.x && p.loc.y + 24. == self.m.y {
+                    return;
+                }
+            }
+            self.piece_remove('r');
+            p = self.black.get_mut(&i).unwrap();
         }
+
         // 更新坐标
         let vec = Vec2::new(self.m.x - 24., self.m.y - 24.);
-        p.vec = vec;
+        p.loc = vec;
         // 恢复状态
         self.state = false;
         self.player += 1;
-        Some(())
     }
-    fn piece_find_and_delect(&mut self, color: char) {
+    // 移除棋子至视界范围外
+    fn piece_remove(&mut self, color: char) {
         let p;
         if color == 'r' {
             p = self.red.iter_mut();
@@ -121,8 +132,8 @@ impl Game {
             p = self.black.iter_mut();
         }
         for (_, p) in p {
-            if p.vec.x + 24. == self.m.x && p.vec.y + 24. == self.m.y {
-                p.vec = Vec2::new(-60., -60.);
+            if p.loc.x + 24. == self.m.x && p.loc.y + 24. == self.m.y {
+                p.loc = Vec2::new(-60., -60.);
             }
         }
     }
@@ -143,8 +154,8 @@ impl EventHandler for Game {
         let b = &self.black;
         let r = &self.red;
         for id in 0..16 {
-            graphics::draw(ctx, &r[&id].img, (r[&id].vec, 0.0, Color::WHITE))?;
-            graphics::draw(ctx, &b[&id].img, (b[&id].vec, 0.0, Color::WHITE))?;
+            graphics::draw(ctx, &r[&id].img, (r[&id].loc, 0.0, Color::WHITE))?;
+            graphics::draw(ctx, &b[&id].img, (b[&id].loc, 0.0, Color::WHITE))?;
         }
         // 绘制选定框at
         graphics::draw(
@@ -166,16 +177,19 @@ impl EventHandler for Game {
     ) {
         let x = (_x + 30.) as i32 / 60;
         let y = (_y + 30.) as i32 / 60;
+
+        // println!("x:{}\ty:{}",x,y);
         // 防止选定框出界
         if x > 0 && x < 10 && y > 0 && y < 11 {
             self.m.x = x as f32 * 60.;
             self.m.y = y as f32 * 60.;
+            println!("x:{}\ty:{}", self.m.x, self.m.y);
         }
         // 棋子移动
         match self.player % 2 {
             0 => {
                 if !self.state {
-                    self.piece_select('r');
+                    self.piece_find('r');
                 } else {
                     self.piece_move('r');
                 }
@@ -183,7 +197,7 @@ impl EventHandler for Game {
             }
             1 => {
                 if !self.state {
-                    self.piece_select('b');
+                    self.piece_find('b');
                 } else {
                     self.piece_move('b');
                 }
