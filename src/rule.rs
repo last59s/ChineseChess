4f9bd9e19b7fa9a3a423bde2fbb2fa38538e44d1
last59s <last59s@hotmail.com>
2022-06-12 19:43:00 +0800

    炮

--- src/rule.rs
+++ src/rule.rs
@@ -15,17 +15,16 @@ static mut REC: [[char; 9]; 10] = [
 ];
 pub fn update_rec(color: char, loc: &Vec2, m: &Vec2) {
     unsafe {
-        println!("loc:\tx:{}\t,y:{}", loc.x, loc.y);
-        println!("m:\tx:{}\t,y:{}", m.x, m.y);
+        // println!("loc:\tx:{}\t,y:{}", loc.x, loc.y);
+        // println!("m:\tx:{}\t,y:{}", m.x, m.y);
         let mut x = loc.x / 60.;
         let mut y = loc.y / 60.;
         REC[y as usize][x as usize] = '+';
-        println!("原：\tx:{}\ty:{}", x as usize, y as usize);
-        // println!("x:{}\ty:{}", x, y);
+        // println!("原：\tx:{}\ty:{}", x as usize, y as usize);
         x = m.x / 60. - 1.;
         y = m.y / 60. - 1.;
         REC[y as usize][x as usize] = color;
-        println!("后：x:{}\ty:{}", x, y);
+        // println!("后：x:{}\ty:{}", x, y);
     }
 }
 
@@ -36,6 +35,8 @@ impl Game {
             1..=2 => Some(self.shi(id)),
             3..=4 => Some(self.xiang(id)),
             5..=6 => Some(self.ma(id)),
+            1..=8 => Some(self.che(id)),
+            9..=10 => Some(self.pao(id)),
             11..=15 => Some(self.bing(id)),
             _ => None,
         };
@@ -82,16 +83,140 @@ impl Game {
         self.is_friend(&mut v);
         v
     }
-    // pub fn pao(&self, id: u8) -> Vec<Vec2>  {
-    //     let mut v: Vec<Vec2> = Vec::new();
-
-    //     v
-    // }
-    // pub fn che(&self, id: u8)  -> Vec<Vec2> {
-    //     let mut v: Vec<Vec2> = Vec::new();
-
-    //     v
-    // }
+    pub fn pao(&self, id: u8) -> Vec<Vec2> {
+        let mut v: Vec<Vec2> = Vec::new();
+        let mut c = '+'; // 对方棋子颜色
+        let p = if self.select.1 == 'r' {
+            c = 'b';
+            Some(self.red.get(&id).unwrap())
+        } else if self.select.1 == 'b' {
+            c = 'r';
+            Some(self.black.get(&id).unwrap())
+        } else {
+            None
+        };
+        if let Some(p) = p {
+            unsafe {
+                let x = (p.loc.x / 60.) as i32;
+                let y = (p.loc.y / 60.) as i32;
+                let mut _state = false;
+                for i in (0..x).rev() { // 左
+                    if REC[y as usize][i as usize] != '+' {
+                        if REC[y as usize][i as usize] == c && _state == true {
+                            v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                            _state = false;
+                            break;
+                        }
+                        _state = true;
+                    }
+                    if _state == false {
+                        v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                    }
+                }
+                _state = false;
+                for i in x + 1..9 {     //右
+                    if REC[y as usize][i as usize] != '+' {
+                        if REC[y as usize][i as usize] == c && _state == true {
+                            v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                            _state = false;
+                            break;
+                        }
+                        _state = true;
+                    }
+                    if _state == false {
+                        v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                    }
+                }
+                _state = false;
+                for i in (0..y).rev() { // 上
+                    if REC[i as usize][x as usize] != '+' {
+                        if REC[i as usize][x as usize] == c && _state == true {
+                            v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                            println!("3-vec1\tx:{}y:{}",p.loc,((i + 1) * 60 - 24) as f32);
+                            _state = false;
+                            break;
+                        }
+                        _state = true;
+                    }
+                    if _state == false {
+                        println!("3-vec2\tx:{}y:{}",p.loc,((i + 1) * 60 - 24) as f32);
+                        v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                    }
+                }
+                _state = false;
+                for i in y + 1..10 {    // 下
+                    if REC[i as usize][x as usize] != '+' {
+                        if REC[i as usize][x as usize] == c && _state == true {
+                            v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                            _state = false;
+                            break;
+                        }
+                        _state = true;
+                    }
+                    if _state == false {
+                        v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                    }
+                }
+            }
+        }
+        v
+    }
+    pub fn che(&self, id: u8) -> Vec<Vec2> {
+        let mut v: Vec<Vec2> = Vec::new();
+        let mut c = '+'; // 对方棋子颜色
+        let p = if self.select.1 == 'r' {
+            c = 'b';
+            Some(self.red.get(&id).unwrap())
+        } else if self.select.1 == 'b' {
+            c = 'r';
+            Some(self.black.get(&id).unwrap())
+        } else {
+            None
+        };
+        if let Some(p) = p {
+            unsafe {
+                let x = (p.loc.x / 60.) as i32;
+                let y = (p.loc.y / 60.) as i32;
+                for i in (0..x).rev() {
+                    if REC[y as usize][i as usize] == p.color {
+                        break;
+                    } else if REC[y as usize][i as usize] == c {
+                        v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                        break;
+                    }
+                    v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                }
+                for i in x + 1..9 {
+                    if REC[y as usize][i as usize] == p.color {
+                        break;
+                    } else if REC[y as usize][i as usize] == c {
+                        v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                        break;
+                    }
+                    v.push(Vec2::new(((i + 1) * 60 - 24) as f32, p.loc.y));
+                }
+                for i in (0..y).rev() {
+                    if REC[i as usize][x as usize] == p.color {
+                        break;
+                    } else if REC[i as usize][x as usize] == c {
+                        v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                        break;
+                    }
+                    v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                }
+                for i in y + 1..10 {
+                    if REC[i as usize][x as usize] == p.color {
+                        break;
+                    } else if REC[i as usize][x as usize] == c {
+                        v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                        break;
+                    }
+                    v.push(Vec2::new(p.loc.x, ((i + 1) * 60 - 24) as f32));
+                }
+            }
+        }
+        v
+    }
     pub fn ma(&self, id: u8) -> Vec<Vec2> {
         let mut v: Vec<Vec2> = Vec::new();
         unsafe {
@@ -99,9 +224,9 @@ impl Game {
                 ([-1, 2], [1, 2]),
                 ([2, 1], [2, -1]),
                 ([-2, 1], [-2, -1]),
-                ([1, -2], [-1, -2]),    
+                ([1, -2], [-1, -2]),
             ];
-            let obstacle = [[0, 1], [1, 0], [-1, 0], [0, -1]];  // 下，右，左，上
+            let obstacle = [[0, 1], [1, 0], [-1, 0], [0, -1]]; // 下，右，左，上
             let p = if self.select.1 == 'r' {
                 Some(self.red.get(&id).unwrap())
             } else if self.select.1 == 'b' {
@@ -188,7 +313,7 @@ impl Game {
                     }
                 }
             }
-            _ => println!("Error in shuai."),
+            _ => println!("Error in shi."),
         }
         for loc in v.iter_mut() {
             if loc.x + 24. < 240. || loc.x + 24. > 360. {
